76ade00ff71dec607b0d6acd8d2e88e0eb2fd5d8
Denise Baker <denviol@yahoo.com>
2019-10-05 12:14:29 -0500

    added card styling based on house

--- src/javascripts/components/studentList/studentList.js
+++ src/javascripts/components/studentList/studentList.js
@@ -1,5 +1,6 @@
 import utilities from '../../helpers/utilities';
 import studentData from '../../helpers/data/studentData';
+import houseData from '../../helpers/data/houseData';
 
 
 import './studentList.scss';
@@ -7,9 +8,14 @@ import studentCard from '../studentCard/studentCard';
 
 const createStudentList = () => {
   const students = studentData.getStudents();
+  const houses = houseData.getHouses();
   let domString = '<ul class="student-list">';
   for (let i = 0; i < students.length; i += 1) {
-    domString += studentCard.createStudentCard(students[i]);
+    for (let j = 0; j < houses.length; j += 1) {
+      if (students[i].houseId === houses[j].id) {
+        domString += studentCard.createStudentCard(students[i], houses[j]);
+      }
+    }
   }
   domString += '</ul>';
   utilities.printToDom(domString, 'student-container');
